c7885e069f0aa56de8de0283f6eef93f770da3db
Kolade <olubanjok@gmail.com>
2025-09-14 22:23:27 +0100

    chore: rename home folder to board

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import LoginPage from "./modules/auth/login";
 import SignupPage from "./modules/auth/signup";
-import HomePage from "./modules/home";
+import HomePage from "./modules/board";
 
 function App() {
   return (
